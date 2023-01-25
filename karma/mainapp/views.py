f36bb63a4344c1ed79dbea88f227749679324486
alexagapkin <alexthemaggot23@gmail.com>
2023-01-25 17:01:13 +0500

    Karma MVP v1.0.0
    - add index page
    - add ru index page

--- karma/mainapp/views.py
+++ karma/mainapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    if '/ru/' in request.path:
+        template = 'mainapp/index_ru.html'
+    else:
+        template = 'mainapp/index.html'
+    return render(request, template)
